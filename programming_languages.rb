6b9cdb8a556b8194e0f9e06040b3e9bb5a72ce99
dison ruan <dison.ruan@yahoo.com>
2017-02-08 15:34:01 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
   # your code here
+  reformed = {}
+  languages.each do |key,value|
+   value.each do |lang, setting|
+     if reformed[lang] == nil
+       reformed[lang] = setting 
+       reformed[lang][:style] = [key]
+     else
+       reformed[lang][:style] << key
+     end
+    end
+   end
+   reformed
 end
